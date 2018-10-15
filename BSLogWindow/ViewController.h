03e95e34043629d85c9712d9b8d0e6d10190d37c
Baishun Xu <>
2018-10-15 19:15:01 +0800

    Initial Commit

---
+++ BSLogWindow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSLogWindow
+//
+//  Created by BaiShun on 2018/10/15.
+//  Copyright © 2018 BaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
